77f5badc0a1feeb684e9c2a94db3017d82fb7128
ahmedbony <ahmed.bony@northsouth.edu>
2018-11-18 13:42:45 +0600

    part 6 upto account and login manager

--- flaskblog/flaskblog/__init__.py
+++ flaskblog/flaskblog/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0d1f445a30617086e3ae2404ba21dc6a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_mananger.login_view = 'login'
 
 from flaskblog import routes
