28da9a5d42fc05a1b2f36ac38c3e8cf6f12b1c8f
ahmedbony <ahmed.bony@northsouth.edu>
2018-11-19 13:01:01 +0600

    bug fixed and upto part 6

--- flaskblog/flaskblog/__init__.py
+++ flaskblog/flaskblog/__init__.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0d1f445a30617086e3ae2404ba21dc6a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_mananger.login_view = 'login'
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
